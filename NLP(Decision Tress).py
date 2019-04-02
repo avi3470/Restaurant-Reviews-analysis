936ceb53b287f8b188ebcd03402772e0c77455ed
Avinash Gupta <1606263@kiit.ac.in>
2019-04-03 02:20:13 +0530

    Add files via upload

---
+++ NLP(Decision Tress).py
@@ -0,0 +1,71 @@
+# Natural Language Processing Using Random Forest Model
+
+# Importing the basic libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+# Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []  #creating empty list it will contain all the 1000 cleaning reviews
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# Creating the Bag of Words model contain all different unique words and one column for each words so basically we will get table rows corrospond to reviews  and column different words 
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
+#calculating Accuracy Precision recall and F1score 
+
+Accuracy = (74+77)/200
+Accuracy 
+
+Precision =74/(74+23)
+Precision
+ 
+Recall = 74/ (74 + 26)
+Recall
+
+F1Score = (2 * Precision * Recall )/ (Precision + Recall)
+
+F1Score
+
